9876eeecbae3818b5aed130dda9fbedd6376569c
Manjot <manjotsingh1901@gmail.com>
2023-06-03 16:04:14 +0530

    Inserting of user data

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
-const router = express.Router();
+const mongodb = require("mongodb");
 
 const db = require("../data/database");
+
+const ObjectId = mongodb.ObjectId;
+
+const router = express.Router();
 
 router.get("/", function (req, res) {
   res.redirect("/posts");
@@ -15,6 +18,30 @@ router.get("/posts", function (req, res) {
 router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+
+  res.redirect("/posts");
 });
 
 module.exports = router;
